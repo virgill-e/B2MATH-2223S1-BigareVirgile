e724a794c48c4e190b3cfb8cff23d2ca131bf981
Virgile BIGARÉ <v.bigare@student.helmo.be>
2023-04-19 11:35:23 +0200

    les enfants enregistrés dans un tableau 46M mot

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -1,15 +1,17 @@
 package tree;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Node {
 	private final char letter;
 	private boolean isFinal;
-	private HashMap<Character, Node> childs;
-	
+	//private HashMap<Character, Node> childs;
+	private Node [] childs;
 	public Node(char letter) {
 		this.letter=letter;
-		childs=new HashMap<>();
+		//childs=new HashMap<>();
+		childs=new Node[0];
 	}
 	
 	public char getLetter() {
@@ -25,11 +27,19 @@ public class Node {
 	}
 
 	public Node getChild(char c) {
-		return this.childs.get(Character.valueOf(c));
+		if(childs.length==0)return null;
+		for(Node child:childs) {
+			if(child.getLetter()==c)return child;
+		}
+		return null;
+		//return this.childs.get(Character.valueOf(c));
 	}
 
 	public void addChild(Node child) {
-		this.childs.put(Character.valueOf(child.getLetter()), child);
+		if(getChild(child.getLetter())!=null)return;
+		childs=Arrays.copyOf(childs, childs.length+1);
+		childs[childs.length-1]=child;
+		//this.childs.put(Character.valueOf(child.getLetter()), child);
 		
 	}
 }
