2349c50a4969fc2592b31e054215bfbb4cbfa637
Virgile Bigaré <virgile.bigare@gmail.com>
2023-05-03 17:44:16 +0200

    constructeur du cryptanalysis fait :)

--- src/cryptanalysis/DictionaryBasedAnalysis.java
+++ src/cryptanalysis/DictionaryBasedAnalysis.java
@@ -5,21 +5,41 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import tree.LexicographicTree;
 
-public class DictionaryBasedAnalysis {
+public class DictionaryBasedAnalysis{
 	
 	private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static final String DICTIONARY = "mots/dictionnaire_FR_sans_accents.txt";
 	
 	private static final String CRYPTOGRAM_FILE = "txt/Plus fort que Sherlock Holmes (cryptogram).txt";
 	private static final String DECODING_ALPHABET = "VNSTBIQLWOZUEJMRYGCPDKHXAF"; // Sherlock
+	private static final Pattern PATTERN_ALL_WORD = Pattern.compile("(\\w+)");
+	private static final Comparator<String> COMP_STRING_BY_LENGTH = (word1, word2) -> word2.length() - word1.length();
 
+	private List<String> words;
+	private final LexicographicTree dict;
 	/*
 	 * CONSTRUCTOR
 	 */
-	public DictionaryBasedAnalysis(String cryptogram, LexicographicTree dict) {
-		// TODO
+	public DictionaryBasedAnalysis(String cryptogram, LexicographicTree dict){
+		this.dict=dict;
+        words= new ArrayList<String>(Arrays.asList(cryptogram.split(" ")))
+        		  .stream()
+        		  .filter(word -> PATTERN_ALL_WORD.matcher(word).matches() &&word.length()>=3)
+        		  .map(String::trim)
+        		  .distinct()
+        		  .sorted(COMP_STRING_BY_LENGTH)
+        		  .collect(Collectors.toList());
 	}
 	
 	/*
@@ -42,7 +62,8 @@ public class DictionaryBasedAnalysis {
 	 * @return The substituted text
 	 */
 	public static String applySubstitution(String text, String alphabet) {
-		return ""; // TODO
+		//TODO: parcourir toute les lettres du texte (remplace remplace les lettre deja remplacé)
+		
 	}
 	
 	/*
