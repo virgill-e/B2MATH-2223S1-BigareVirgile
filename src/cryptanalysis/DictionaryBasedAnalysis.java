ac50d8df4bcc7bcb0c27b3936d26dec509f5faca
Virgile Bigaré <virgile.bigare@gmail.com>
2023-05-03 21:36:48 +0200

    applySubstitution fait !

--- src/cryptanalysis/DictionaryBasedAnalysis.java
+++ src/cryptanalysis/DictionaryBasedAnalysis.java
@@ -16,11 +16,11 @@ import java.util.stream.Stream;
 
 import tree.LexicographicTree;
 
-public class DictionaryBasedAnalysis{
-	
+public class DictionaryBasedAnalysis {
+
 	private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static final String DICTIONARY = "mots/dictionnaire_FR_sans_accents.txt";
-	
+
 	private static final String CRYPTOGRAM_FILE = "txt/Plus fort que Sherlock Holmes (cryptogram).txt";
 	private static final String DECODING_ALPHABET = "VNSTBIQLWOZUEJMRYGCPDKHXAF"; // Sherlock
 	private static final Pattern PATTERN_ALL_WORD = Pattern.compile("(\\w+)");
@@ -28,26 +28,25 @@ public class DictionaryBasedAnalysis{
 
 	private List<String> words;
 	private final LexicographicTree dict;
+
 	/*
 	 * CONSTRUCTOR
 	 */
-	public DictionaryBasedAnalysis(String cryptogram, LexicographicTree dict){
-		this.dict=dict;
-        words= new ArrayList<String>(Arrays.asList(cryptogram.split(" ")))
-        		  .stream()
-        		  .filter(word -> PATTERN_ALL_WORD.matcher(word).matches() &&word.length()>=3)
-        		  .map(String::trim)
-        		  .distinct()
-        		  .sorted(COMP_STRING_BY_LENGTH)
-        		  .collect(Collectors.toList());
+	public DictionaryBasedAnalysis(String cryptogram, LexicographicTree dict) {
+		this.dict = dict;
+		words = new ArrayList<String>(Arrays.asList(cryptogram.split(" "))).stream()
+				.filter(word -> PATTERN_ALL_WORD.matcher(word).matches() && word.length() >= 3).map(String::trim)
+				.distinct().sorted(COMP_STRING_BY_LENGTH).collect(Collectors.toList());
 	}
-	
+
 	/*
 	 * PUBLIC METHODS
 	 */
 
 	/**
-	 * Performs a dictionary-based analysis of the cryptogram and returns an approximated decoding alphabet.
+	 * Performs a dictionary-based analysis of the cryptogram and returns an
+	 * approximated decoding alphabet.
+	 * 
 	 * @param alphabet The decoding alphabet from which the analysis starts
 	 * @return The decoding alphabet at the end of the analysis process
 	 */
@@ -57,20 +56,33 @@ public class DictionaryBasedAnalysis{
 
 	/**
 	 * Applies an alphabet-specified substitution to a text.
-	 * @param text A text
+	 * 
+	 * @param text     A text
 	 * @param alphabet A substitution alphabet
 	 * @return The substituted text
 	 */
 	public static String applySubstitution(String text, String alphabet) {
-		//TODO: parcourir toute les lettres du texte (remplace remplace les lettre deja remplacé)
-		
+		String result = "";
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+			if (c == ' ' || c == '\n') {
+				result += c;
+			} else {
+				int index = LETTERS.indexOf(c);
+				if (index != -1) {
+					result += alphabet.charAt(index);
+				}
+			}
+		}
+		return result;
 	}
-	
+
 	/*
 	 * PRIVATE METHODS
 	 */
 	/**
 	 * Compares two substitution alphabets.
+	 * 
 	 * @param a First substitution alphabet
 	 * @param b Second substitution alphabet
 	 * @return A string where differing positions are indicated with an 'x'
@@ -82,9 +94,10 @@ public class DictionaryBasedAnalysis{
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Load the text file pointed to by pathname into a String.
+	 * 
 	 * @param pathname A path to text file.
 	 * @param encoding Character set used by the text file.
 	 * @return A String containing the text in the file.
@@ -99,11 +112,11 @@ public class DictionaryBasedAnalysis{
 		}
 		return data;
 	}
-	
-    /*
+
+	/*
 	 * MAIN PROGRAM
 	 */
-	
+
 	public static void main(String[] args) {
 		/*
 		 * Load dictionary
@@ -112,7 +125,7 @@ public class DictionaryBasedAnalysis{
 		LexicographicTree dict = new LexicographicTree(DICTIONARY);
 		System.out.println("done.");
 		System.out.println();
-		
+
 		/*
 		 * Load cryptogram
 		 */
@@ -121,20 +134,20 @@ public class DictionaryBasedAnalysis{
 //		System.out.println();
 
 		/*
-		 *  Decode cryptogram
+		 * Decode cryptogram
 		 */
 		DictionaryBasedAnalysis dba = new DictionaryBasedAnalysis(cryptogram, dict);
 		String startAlphabet = LETTERS;
 //		String startAlphabet = "ZISHNFOBMAVQLPEUGWXTDYRJKC"; // Random alphabet
 		String finalAlphabet = dba.guessApproximatedAlphabet(startAlphabet);
-		
+
 		// Display final results
 		System.out.println();
 		System.out.println("Decoding     alphabet : " + DECODING_ALPHABET);
 		System.out.println("Approximated alphabet : " + finalAlphabet);
 		System.out.println("Remaining differences : " + compareAlphabets(DECODING_ALPHABET, finalAlphabet));
 		System.out.println();
-		
+
 		// Display decoded text
 		System.out.println("*** DECODED TEXT ***\n" + applySubstitution(cryptogram, finalAlphabet).substring(0, 200));
 		System.out.println();
